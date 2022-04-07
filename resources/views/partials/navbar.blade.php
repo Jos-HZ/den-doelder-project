ea4e72026638f54e01f06ec754e1edb35f7743ce
Luthor <kvherwijnen@hotmail.com>
2022-04-07 18:53:37 +0200

    frontend

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,21 +1,35 @@
-<nav class="navbar is-dark  has-text-white" style="background: #F0F0F0 ">
+<nav class="navbar is-dark  has-text-white">
     <div class="container">
         <div class="navbar-brand">
             <a href="/" class="navbar-item">
                 <img src="./img/logo-den-doelder.jpeg" alt="Logo Den Doelder">
             </a>
-            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
         </div>
         <div class="navbar-menu" id="navMenu">
             <div class="navbar-start">
-                <a class="{{ Request::path() === 'orders' ? 'active' : '' }}" href="{{ url('/order') }}">Order</a>
-                <a class="{{ Request::path() === 'backlog' ? 'active' : '' }}" href="{{ url('/backlog') }}">Back-log</a>
-                <a class="{{ Request::path() === 'qualityControl' ? 'active' : '' }}" href="{{ url('/qualityControl') }}">Quality Control</a>
+                <a class="navbar-item {{ Request::path() === 'orders' ? 'active' : '' }}" href="{{ url('/order') }}">
+                    Order
+                </a>
+                <a class="navbar-item {{ Request::path() === 'backlog' ? 'active' : '' }}" href="{{ url('/backlog') }}">
+                    Back-log
+                </a>
+                <a class="navbar-item {{ Request::path() === 'qualityControl' ? 'active' : '' }}" href="{{ url('/qualityControl') }}">
+                    Quality Control
+                </a>
             </div>
         </div>
     </div>
 </nav>
+
+{{--<a href="{{ route('orders.index') }}"--}}
+{{--   class="navbar-item {{ Request::route()->getName() === 'orders.index' ? "is-active" : "" }}">--}}
+{{--    Orders--}}
+{{--</a>--}}
+{{--<a href="{{ route('backlog.index') }}"--}}
+{{--   class="navbar-item {{ Request::route()->getName() === 'backlog.index' ? "is-active" : "" }}">--}}
+{{--    Backlog--}}
+{{--</a>--}}
+{{--<a href="{{ route('qualityControl.index') }}"--}}
+{{--   class="navbar-item {{ Request::route()->getName() === 'qualityControl.index' ? "is-active" : "" }}">--}}
+{{--    Quality--}}
+{{--</a>--}}
