31b11baae224ae4adf400425af7869e6c80d7935
Luthor <kvherwijnen@hotmail.com>
2022-04-12 17:29:51 +0200

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,28 +1,45 @@
 @extends('layout.master')
 
 @section('content')
+    {{--            TODO make dynamic --}}
     <section class="section">
         <div class="container">
-            <div class="tabs is-boxed" role="tab">
+
+            <!-- Nav tabs -->
+            <div class="tabs is-centered" role="tabpanel">
                 <ul role="tablist">
-                    @foreach($productions as $production)
-                        <li class="{{ $production->id === 1 ? 'active' : '' }}">
-                            <a href="#{{ $production -> production_line }}" aria-controls="{{ $production->production_line }}" id="{{ $production->production_line }}" role="tab" data-toggle="tab">{{ $production -> production_line }}</a>
-                        </li>
-                    @endforeach
+                    @if($productions)
+                        @foreach($productions as $production)
+                            <li role="presentation" class="active">
+                                <a href="#{{ $production -> production_line }}"
+                                   aria-controls="{{ $production->production_line }}"
+                                   id="{{ $production->production_line }}"
+                                   role="tab"
+                                   data-toggle="tab">{{ $production -> production_line }}
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
                 </ul>
             </div>
 
-{{--            TODO make dynamic --}}
-            @foreach($orders as $order)
-                <a href="{{ route('order.show', $order) }}">
-                    <article class="tile is-child notification has-background-grey-lighter">
-                        <p class="title text-bold">order {{ $order -> ordernumber }}</p>
-                        <p class="subtitle has text-success"> this is the note section</p>
-                    </article>
-                </a>
-                <br>
-            @endforeach
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    @if($orders)
+                        @foreach($orders as $order)
+                            <div role="tabpanel" class="tab-pane active" id="{{ $production->production_line }}">
+                            <a href="{{ route('order.show', $order) }}">
+                                <article class="tile is-child notification has-background-grey-lighter">
+                                    <p class="title text-bold">order {{ $order -> ordernumber }}</p>
+                                    <p class="subtitle has text-success"> this is the note section</p>
+                                </article>
+                            </a>
+                            </div>
+                        @endforeach
+                    @endif
+
+                </div>
+
         </div>
     </section>
 @endsection
