ad4abc3d5b2145547a3b886d76c0a088065251b6
Luthor <kvherwijnen@hotmail.com>
2022-04-13 08:46:17 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,40 +3,21 @@
 @section('content')
     <section class="section">
         <div class="container">
-            <div class="columns">
-
-                <div class="column is-8-desktop is-12-tablet">
-
-                    <div class="content">
-                        <h1>Welcome to the Den doelder application</h1>
-                        <div class=" is-ancestor">
-                            <div class="tile is-parent">
-                                <article class="tile is-child notification is-warning">
-                                    <p class="title">Control list</p>
-                                </article>
-                            </div>
-                            <div class="tile is-parent">
-                                <article class="tile is-child notification is-success">
-                                    <p class="title">Pallet design</p>
-                                </article>
-                            </div>
-                            <div class="tile is-parent">
-                                <article class="tile is-child notification is-info">
-                                    <p class="title">Quality control</p>
-                                </article>
-                            </div>
-                            <div class="tile is-parent">
-                                <article class="tile is-child notification is-primary">
-                                    <p class="title">Error</p>
-                                </article>
-                            </div>
-                            <div class="tile is-parent">
-                                <article class="tile is-child notification is-warning">
-                                    <p class="title">Notes</p>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
+            <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title text-lg-center">Admin</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title text-lg-center">Driver</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title text-lg-center">Production</p>
+                    </article>
                 </div>
             </div>
         </div>
