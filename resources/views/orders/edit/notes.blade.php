868fb8d52e576d00879389391b3dc8a2afde8db6
Jos-HZ <90176784+Jos-HZ@users.noreply.github.com>
2022-04-14 10:08:49 +0200

    Notes textbox now autogrows

--- resources/views/orders/edit/notes.blade.php
+++ resources/views/orders/edit/notes.blade.php
@@ -14,7 +14,19 @@
                 <form id="notes" method="post" action="edit">
                     @csrf
                     @method ('put')
-                    <textarea type="text" id="notes" name="notes">{{ $order->notes }}</textarea>
+                    {{-- <textarea type="text" id="notes" name="notes" oninput="this.style.height = this.scrollHeight + 'px'">{{ $order->notes }}</textarea> --}}
+                    {{-- <textarea type="text" id="notes" name="notes">{{ $order->notes }}</textarea> --}}
+                    <div class="grow-wrap">
+                        <textarea name="notes" oninput="this.parentNode.dataset.replicatedValue = this.value" class="is-focused has-background-success">{{ $order->notes }}</textarea>
+                        <script>document.querySelector('#notes > div > textarea').parentNode.dataset.replicatedValue = document.querySelector('#notes > div > textarea').value;</script>
+                        {{-- <textarea name="notes" oninput="this.parentNode.dataset.replicatedValue = this.value">{{ $order->notes }}</textarea>
+                        <script>
+                            const textarea = document.querySelector('#notes > div > textarea');
+                            textarea.parentNode.dataset.replicatedValue = textarea.value;
+                            textarea.className = 'is-focused has-background-success';
+                            textarea.parentNode.dataset.className = 'textarea is-focused has-background-success';
+                        </script> --}}
+                    </div>
                 </form>
             </div>
             {{-- <form action="{{ Request::url() }}/edit?field=notes">
