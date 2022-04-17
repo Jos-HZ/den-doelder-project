ac103ea2a99941f93418400660b4e7f2fc3a5bff
Ivy Dekker <ivy_dk@hotmail.com>
2022-04-17 17:30:05 +0200

    Update tabs.js
    changed var

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,10 +1,9 @@
 function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content-tab");
-    for (i = 0; i < x.length; i++) {
+    const x = document.getElementsByClassName("content-tab");
+    for (let i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tab");
+    const tablinks = document.getElementsByClassName("tab");
     for (i = 0; i < x.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" is-active", "");
     }
