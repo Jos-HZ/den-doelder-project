f13cfa6ca522c1296d96b9e9bc07100a52d44f59
Ivy Dekker <ivy_dk@hotmail.com>
2022-04-17 17:16:38 +0200

    tabs work

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,62 +1,13 @@
-console.log('tab.js is working')
-
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('[role="tab"]');
-    const tabList = document.querySelector('[role="tablist"]');
-
-    // Add a click event handler to each tab
-    tabs.forEach(tab => {
-        tab.addEventListener('click', changeTabs);
-    });
-
-    // Enable arrow navigation between tabs in the tab list
-    let tabFocus = 0;
-
-    tabList.addEventListener('keydown', e => {
-        // Move right
-        if (e.keyCode === 39 || e.keyCode === 37) {
-            tabs[tabFocus].setAttribute('tabindex', -1);
-            if (e.keyCode === 39) {
-                tabFocus++;
-                // If we're at the end, go to the start
-                if (tabFocus >= tabs.length) {
-                    tabFocus = 0;
-                }
-                // Move left
-            } else if (e.keyCode === 37) {
-                tabFocus--;
-                // If we're at the start, move to the end
-                if (tabFocus < 0) {
-                    tabFocus = tabs.length - 1;
-                }
-            }
-
-            tabs[tabFocus].setAttribute('tabindex', 0);
-            tabs[tabFocus].focus();
-        }
-    });
-});
-
-function changeTabs(e) {
-    const target = e.target;
-    const parent = target.parentNode;
-    const grandparent = parent.parentNode;
-
-    // Remove all current selected tabs
-    parent
-        .querySelectorAll('[aria-selected="true"]')
-        .forEach(t => t.setAttribute('aria-selected', false));
-
-    // Set this tab as selected
-    target.setAttribute('aria-selected', true);
-
-    // Hide all tab panels
-    grandparent
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach(p => p.setAttribute('hidden', true));
-
-    // Show the selected panel
-    grandparent.parentNode
-        .querySelector(`#${target.getAttribute('aria-controls')}`)
-        .removeAttribute('hidden');
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("content-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " is-active";
 }
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -8,56 +8,60 @@
     <section class="section">
         <div class="container">
 
-            <div class="tabs is-centered">
-                <!-- Nav tabs -->
-                <div role="tablist" aria-label="Cape tabs">
-                    <span role="tab" aria-selected="true" aria-controls="panel-1" id="tab-1" tabindex="0">
-                        Cape 1
-                    </span>
-                    <span role="tab" aria-selected="false" aria-controls="panel-2" id="tab-2" tabindex="-1">
-                        Cape 2
-                    </span>
-                    <span role="tab" aria-selected="false" aria-controls="panel-3" id="tab-3" tabindex="-1">
-                        Cape 5
-                    </span>
-                </div>
+            <div class="hero-foot">
+                <nav class="tabs is-boxed is-fullwidth is-large">
+                    <div class="container">
+                        <ul>
+                            <li class="tab is-active" onclick="openTab(event,'cape-1')"><a>Cape 1</a></li>
+                            <li class="tab" onclick="openTab(event,'cape-2')"><a>Cape 2</a></li>
+                            <li class="tab" onclick="openTab(event,'cape-5')"><a>Cape 3</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
 
-                <!-- Tab content -->
-                <div id="panel-1" role="tabpanel" tabindex="0" aria-labelledby="tab-1">
+            <div class="container section">
+                <div id="cape-1" class="content-tab">
                     @foreach($orders as $order)
                         @if($order->production_id === 1)
-                            <a href="{{ route('order.show', $order) }}">
+                            <div class="my-3">
+                                <a href="{{ route('order.show', $order) }}">
                                     <article class="tile is-child notification has-background-grey-lighter">
                                         <p class="title text-bold">order {{ $order -> ordernumber }}</p>
                                         <p class="subtitle has text-success"> this is the note section</p>
                                     </article>
-                            </a>
+                                </a>
+                            </div>
                         @endif
                     @endforeach
                 </div>
 
-                <div id="panel-2" role="tabpanel" tabindex="0" aria-labelledby="tab-2" hidden>
+                <div id="cape-2" class="content-tab" style="display:none">
                     @foreach($orders as $order)
                         @if($order->production_id === 2)
-                            <a href="{{ route('order.show', $order) }}">
-                                <article class="tile is-child notification has-background-grey-lighter">
-                                    <p class="title text-bold">order {{ $order -> ordernumber }}</p>
-                                    <p class="subtitle has text-success"> this is the note section</p>
-                                </article>
-                            </a>
+                            <div class="my-3">
+                                <a href="{{ route('order.show', $order) }}">
+                                    <article class="tile is-child notification has-background-grey-lighter">
+                                        <p class="title text-bold">order {{ $order -> ordernumber }}</p>
+                                        <p class="subtitle has text-success"> this is the note section</p>
+                                    </article>
+                                </a>
+                            </div>
                         @endif
                     @endforeach
                 </div>
 
-                <div id="panel-3" role="tabpanel" tabindex="0" aria-labelledby="tab-3" hidden>
+                <div id="cape-5" class="content-tab" style="display:none">
                     @foreach($orders as $order)
                         @if($order->production_id === 5)
-                            <a href="{{ route('order.show', $order) }}">
-                                <article class="tile is-child notification has-background-grey-lighter">
-                                    <p class="title text-bold">order {{ $order -> ordernumber }}</p>
-                                    <p class="subtitle has text-success"> this is the note section</p>
-                                </article>
-                            </a>
+                            <div class="my-3">
+                                <a href="{{ route('order.show', $order) }}">
+                                    <article class="tile is-child notification has-background-grey-lighter">
+                                        <p class="title text-bold">order {{ $order -> ordernumber }}</p>
+                                        <p class="subtitle has text-success"> this is the note section</p>
+                                    </article>
+                                </a>
+                            </div>
                         @endif
                     @endforeach
                 </div>
