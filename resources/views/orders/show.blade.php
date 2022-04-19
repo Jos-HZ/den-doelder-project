cd6754848123ef6e52e080c4ebb2b7c6f3162b97
Jos-HZ <90176784+Jos-HZ@users.noreply.github.com>
2022-04-19 12:03:44 +0200

    Improved notes editing

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -28,24 +28,29 @@
                             @if(true)
                                 @if (Request::query('field') === 'notes')
                                     <input type="submit" form="notes" value="floppy disk svg"/>
-                                    <div class="content">
-                                        <form id="notes" method="post" action="{{ route('orders.update', $order->id) }}">
-                                            @csrf
-                                            @method('PUT')
-                                            <textarea type="text" id="notes" name="notes">{{ $order->notes }}</textarea>
-                                        </form>
-                                    </div>
                                 @else
                                     <div class="notesButton">
                                         <a href="{{ Request::url() }}/edit?field=notes"><img src="/img/svg/{{ (isset($order->notes) ? 'edit' : 'create') }}.svg"></a>
                                     </div>
-                                    {{--                        TODO add here the note dynamic--}}
-                                    <div class="content">
-                                        <p>{{ $order->notes }}</p>
-                                    </div>
                                 @endif
                             @endif
                         </div>
+                        @if(true)
+                            @if (Request::query('field') === 'notes')
+                                <div class="content">
+                                    <form id="notes" method="post" action="{{ route('orders.update', $order->id) }}">
+                                        @csrf
+                                        @method('PUT')
+                                        <textarea type="text" id="notes" name="notes">{{ $order->notes }}</textarea>
+                                    </form>
+                                </div>
+                            @else
+                                {{--                        TODO add here the note dynamic--}}
+                                <div class="content">
+                                    <p>{{ $order->notes }}</p>
+                                </div>
+                            @endif
+                        @endif
                     </article>
                 </div>
                 <div class="tile is-parent">
