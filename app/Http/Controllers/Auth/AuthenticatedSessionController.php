d02ed2627dcdd1b0d9ff875b1eb42def40340220
Kevin van Herwijnen <kvherwijnen@hotmail.com>
2022-04-19 11:38:24 +0200

    refactor accomplished

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
+    public function index()
+    {
+        return view('auth.login');
+    }
     /**
      * Display the login view.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\BacklogController;
+use App\Http\Controllers\ErrorController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::resource('/', AuthenticatedSessionController::class);
+Route::resource('/order', OrderController::class);
+Route::resource('/backlog', BacklogController::class);
+Route::resource('/error', ErrorController::class);
+
 
 // user dashboard
 Route::get('/dashboard', function () {
