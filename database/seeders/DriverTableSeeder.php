8d24605125ab1cdc3b3c6790df209f632df2409e
Kevin van Herwijnen <kvherwijnen@hotmail.com>
2022-04-19 13:05:31 +0200

    added users to seeder

---
+++ database/seeders/DriverTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DriverTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'driver',
+            'email' => 'driver@driver.com',
+            'password' => bcrypt('driver12345'),
+            'role' => 'driver'
+        ]);
+    }
+}
---
+++ database/seeders/ProductionTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ProductionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'production',
+            'email' => 'production@production.com',
+            'password' => bcrypt('production12345'),
+            'role' => 'production'
+        ]);
+    }
+}
