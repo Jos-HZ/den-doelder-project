c0fe6016729fbb02e82a142d12009f569f61a630
Jos-HZ <90176784+Jos-HZ@users.noreply.github.com>
2022-04-21 10:48:22 +0200

    Active tab now receives the "current" class

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,30 +1,20 @@
-function openTab(evt, tabName) {
-    const x = document.getElementsByClassName("content-tab");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    const tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " is-active";
-}
-
-function XcrollBy(param) {
+function scrollX(tab, absolute, param, event) {
     const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    document.querySelector('container.horizontal.flexContainer').scrollBy({
+    const element = document.querySelector('container.horizontal.flexContainer');
+    const options = {
         top: 0,
         left: param * viewportWidth,
         behavior: 'smooth'
-    });
-}   
-
-function XcrollTo(param) {
-    const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    document.querySelector('container.horizontal.flexContainer').scrollTo({
-        top: 0,
-        left: param * viewportWidth,
-        behavior: 'smooth'
-    });
+    };
+    console.log(999);
+    element.scrollTo(options);
+    new Array().slice.call(tab.parentNode.children).forEach((element, index, array) => { (new Array().slice.call(element.classList).includes('current') || index === param ? element.classList.toggle('current') : '') }); // één regel woohoo
+    // new Array().slice.call(tab.parentNode.children).forEach((element, index, array) => {
+    //     if ((new Array().slice.call(element.classList).includes('current')) || (index === param)) {
+    //         element.classList.toggle('current');
+    //     }
+    // });
+    // new Array().slice.call(tab.parentNode.children).forEach((element, index, array) => {
+    //     console.log(element.classList);
+    // });
 }
