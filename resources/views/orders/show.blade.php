a6715e976f85f98f1ae7145969d2e0cc5fb52452
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-11 11:02:31 +0200

    add quality control button

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -17,11 +17,13 @@
                     </article>
                 </div>
                 <div class="tile is-parent">
+                    <div class="tile is-child box">
                     <a href="{{ route("qualityControl.index", ['ordernumber' => $order->ordernumber]) }}">
-                        <article class="tile is-child box">
+                        <article >
                             <p class="title text-lg-center">Quality control</p>
                         </article>
                     </a>
+                    </div>
 
                 </div>
             </div>
@@ -72,13 +74,22 @@
                         @endif
                     </article>
                 </div>
-                <div class="tile is-parent">
-                    <a href="{{ route('error.create', ['ordernumber' => $order->ordernumber ])}}">
-                        <article class="tile is-child box has-background-danger">
-                            <p class="title text-lg-center">Error</p>
-                        </article>
+                <div class="tile is-parent is-vertical">
+                    <div class="tile is-child box">
+                    <a href="{{ route('qualityControl.create')}}">
+                            <p class="title text-lg-center">Create quality control</p>
                     </a>
+                    </div>
+
+                    <div class="tile is-child box has-background-danger">
+                        <a href="{{ route('error.create', ['ordernumber' => $order->ordernumber ])}}">
+                            <p class="title text-lg-center">Error</p>
+                        </a>
+                    </div>
                 </div>
+
+
+
             </div>
         </div>
     </section>
