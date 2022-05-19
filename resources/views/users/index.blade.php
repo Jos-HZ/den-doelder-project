d5d56e955cd6c9288c60ef0ddc996447953a195a
Kevin van Herwijnen <76037700+kvherwijnen@users.noreply.github.com>
2022-05-19 11:55:28 +0200

    Update index.blade.php
    Admin is not allowed to delete himself

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -29,18 +29,15 @@
                 <td>{{ ++$i }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <td>
-{{--                                        @if(!empty($user->getRoleNames()))--}}
-{{--                                            @foreach($user->getRoleNames() as $v)--}}
-{{--                                                <label class="badge badge-success">{{ $v }}</label>--}}
-{{--                                            @endforeach--}}
-{{--                                        @endif--}}
-                </td>
+                <td>{{$user->role}}</td>
+
                 <td>
                     <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
                     <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
                     {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                    @if($user->role !== "admin")
                     {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                    @endif
                     {!! Form::close() !!}
                 </td>
             </tr>
