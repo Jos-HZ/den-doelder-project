4de12eea5c593bef3f4613637d31073b05af4843
niek726 <schl0014@hz.nl>
2022-06-08 11:16:25 +0200

    delete trash

--- resources/views/partials/language_switcher.blade.php
+++ resources/views/partials/language_switcher.blade.php
@@ -1,15 +1,9 @@
 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
     @foreach($available_locales as $locale_name => $available_locale)
         @if($available_locale === $current_locale)
-{{--            <span class="ml-2 mr-2 text-gray-700">{{ $locale_name }}</span>--}}
         @else
-{{--            <a class="ml-1 underline ml-2 mr-2" href="language/{{ $available_locale }}">--}}
-{{--                <img src="\img\svg\{{ $available_locale }}.svg" >--}}
-{{--                <span>{{ $locale_name }}</span>--}}
-{{--            </a>--}}
-
             <a href="/language/{{ $available_locale }}">
-                <img src= "\img\svg\{{ $available_locale }}.svg" style="width:42px;height:42px;">
+                <img src="\img\svg\{{ $available_locale }}.svg" style="width:42px;height:42px;">
             </a>
         @endif
     @endforeach
