bb757066b03b7212652fa2d9e899852115aa47e1
niek726 <schl0014@hz.nl>
2022-06-08 08:52:55 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <section class="section">
         <div class="container">
             @can('is_admin')
-                <h1>{{__("Je bent ingelogd als admin")}}</h1>
+                <h1>{{__("You are logged in as admin!")}}</h1>
 
                 <div class="tile is-ancestor">
                     <div class="tile is-parent is-vertical">
