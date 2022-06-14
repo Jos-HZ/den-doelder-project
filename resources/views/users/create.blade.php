19f872e449efc68593c6158a02820db1c5e9a695
niek726 <schl0014@hz.nl>
2022-06-14 09:23:43 +0200

    admin language

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -37,7 +37,7 @@
 
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Email:</strong>
+                        <strong>{{__("Email")}}:</strong>
                         {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
                     </div>
                 </div>
