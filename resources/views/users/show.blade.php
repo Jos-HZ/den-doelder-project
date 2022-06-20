9bd939ee91f2fa83e257530e78728fb3639e49ca
ivydk <90151380+ivydk@users.noreply.github.com>
2022-06-20 09:36:25 +0200

    Update show.blade.php

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
     <section class="section">
-        <span>Paris</span>
-        <span class="icon">
-    <i class="fas fa-arrow-right"></i>
-  </span>
-        <span>Budapest</span>
-        <span class="icon">
-    <i class="fas fa-arrow-right"></i>
-  </span>
         <a href="/users"><img src="/img/svg/back-arrow.svg" display="block" width="35" height="35"></a>
         <div class="container">
             <div class="row">
