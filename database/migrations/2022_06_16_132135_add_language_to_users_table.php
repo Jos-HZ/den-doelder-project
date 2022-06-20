8a316e1faad1a26fa319308696b696febac43468
ivydk <90151380+ivydk@users.noreply.github.com>
2022-06-20 09:32:33 +0200

    Update 2022_06_16_132135_add_language_to_users_table.php

--- database/migrations/2022_06_16_132135_add_language_to_users_table.php
+++ database/migrations/2022_06_16_132135_add_language_to_users_table.php
@@ -26,7 +26,7 @@ class AddLanguageToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('language');
         });
     }
 }
