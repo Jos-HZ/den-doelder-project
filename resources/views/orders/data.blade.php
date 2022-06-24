0dcc481d821ffd864f75e61aefd9a7e22c7d7585
Kevin van Herwijnen <76037700+kvherwijnen@users.noreply.github.com>
2022-06-24 15:41:53 +0200

    Update data.blade.php

--- resources/views/orders/data.blade.php
+++ resources/views/orders/data.blade.php
@@ -4,6 +4,7 @@
         <img src="/img/svg/back-arrow.svg" onclick="history.back();" width="35" height="35">
 
         <div class="container">
+            <h1 class="title has-text-centered">Order Log</h1>
             @include('partials.allData')
         </div>
     </section>
